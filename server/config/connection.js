7a9528295fb3e0b27dd08b605e625445c6f72dd2
Cadenza <mirandalucsb@gmail.com>
2022-04-04 12:31:04 -0700

    installed stuff, and made name changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/<your database name>', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/jewelry_design_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
