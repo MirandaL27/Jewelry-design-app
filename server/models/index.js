dd85510ae0362d1708b32b3b5874365b1f9c6fea
Cadenza <mirandalucsb@gmail.com>
2022-04-07 22:50:05 -0700

    added new models to index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,9 @@
 const User = require('./User');
 const Collage = require('./Collage');
 const Color = require("./Color");
+const Country = require("./Country");
 const Component = require('./Component');
+const ComponentType = require('./ComponentType');
 const Design = require('./Design');
 const InspirationalImage = require('./InspirationalImage');
 const JewelryType = require('./JewelryType');
@@ -13,4 +15,4 @@ const Shop = require('./Shop');
 const Stone = require('./Stone');
 const Style = require('./Style');
 
-module.exports = { User, Collage, Color, Component, Design, InspirationalImage, JewelryType, Layout, Logo, Material, Shape, Shop, Stone, Style};
+module.exports = { User, Collage, Color, Country, Component, ComponentType, Design, InspirationalImage, JewelryType, Layout, Logo, Material, Shape, Shop, Stone, Style};
