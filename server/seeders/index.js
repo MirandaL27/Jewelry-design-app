bced0788c40c1326401e67249a1d35e14e76f41e
Cadenza <mirandalucsb@gmail.com>
2022-04-11 23:43:11 -0700

    added seeds to seed file

--- server/seeders/index.js
+++ server/seeders/index.js
@@ -1,59 +1,196 @@
-// const { FreeWeight } = require('../models');
+ const { Color, ComponentType, Country, JewelryType, Layout, Material, Shape, Stone, Style } = require('../models');
 //import all models that need seeds
-///const db = require('../config/connection');
+const db = require('../config/connection');
 
-// db.once('open', async () => {
-//   await FreeWeight.deleteMany({});
-//delete all pervious data in the seeded models
+ db.once('open', async () => {
+     //delete all pervious data in the seeded models
+     Promise.all([Color.deleteMany({user_id: null}), 
+        ComponentType.deleteMany({user_id: null}),
+        Country.deleteMany({user_id: null}),
+        JewelryType.deleteMany({user_id: null}), 
+        Layout.deleteMany({user_id: null}), 
+        Material.deleteMany({user_id: null}), 
+        Shape.deleteMany({user_id: null}), 
+        Stone.deleteMany({user_id: null}), 
+        Style.deleteMany({user_id: null})
+    ]);
 //add seeds
-//   const freeWeightData = [
-//     {
-//       name:"Deadlift",
-//       description: "Stand with feet hip width apart. Bend over and grab the barbell, hands a shoulder’s width apart. Bend your knees slightly bent and your back flat (do not round as this can lead to injury). Gaze about three feet in front of you. Make sure to draw your shoulder blades down and back before lifting. When you lift the bar, imagine trying to push the floor away from you with your heels. Drive your hips forward as you stand. Lower in the same fashion, or drop the bar if weight exceeds ability to lower with control. " ,
-//       image: "placeholder",
-//       link: "deadlift"
-//     },
-//     {
-//       name:"Bench Press",
-//       description: "Lie with your back on the bench and dumbbells in straight arms positioned above your chest. Lower the weights at a 45-degree angle to your body until chest level. Drive both weights back up to the starting position and repeat. If you haven’t bench pressed before, consider using a spotter. Additionally, make sure to research how to get into the proper starting position with a kickback if you are attempting to lift heavy dumbbells as trying to get into position with heavy dumbbells can lead to injury",
-//       image: "placeholder",
-//       link: 'benchpress'
-//     },
-//     {
-//       name:"Bent Over Row",
-//       description: "Place the same side hand and knee on a bench with the weight positioned on the floor next to the bench. Grab the weight and with a flat back, pull the weight toward your chest, engaging your lats and lower traps. Lower and repeat." ,
-//       image: "placeholder",
-//       link: 'bentoverrow'
-//     },
-//     {
-//       name:"Lateral Raise",
-//       description: "Hold the dumbbells at each side. Stand straight with your shoulder blades drawn down and back. Raise the dumbbells simultaneously directly up and out to the side until your hands are at shoulder level. Lower with control and repeat. " ,
-//       image: "placeholder",
-//       link: 'lateralraise'
-//     },
-//     {
-//       name:"Back Squat",
-//       description: "Use the racks to place a barbell at chest level. Step under the bar, squeeze your shoulders back and place the barbell on the meaty part of your drawn back shoulders (your upper trap). With feet hip width apart, stand straight up and walk a few steps forward to clear the rack. Keep your back straight and flat, gaze three feet forward and feet parallel and shoulder’s width apart. Bend your knees and sit back imagining you are about to sit in a chair until your knees are slightly less then 90 degrees. Stand back up by driving your hips forward. " ,
-//       image: "placeholder",
-//       link: 'backsquat'
-//     },
-//     {
-//       name:"Bicep Curl",
-//       description: "Hold the dumbbells in front of your body with your palms facing forward. Starting with your fist, curl your arm forward until you have brought the dumbbell to your shoulder. You can do both arms simultaneously or alternate the weight. Be sure to perform slow and controlled and not use momentum to swing the weight up. " ,
-//       image: "placeholder",
-//       link: 'bicep'
-//     },
-//     {
-//       name:"Military Press",
-//       description: "Hold the dumbbells with arms bent and with one end of each dumbbell resting on the fronts of the shoulders, palms facing inward. Drive the weights straight up overhead while simultaneously twisting your palms to face forward. Reverse the motion and repeat. " ,
-//       image: "placeholder",
-//       link:'militarypress'
-//     }
-    
-//   ];
-//insert everything into the database with the models
-//   const createFreeWeights = await FreeWeight.collection.insertMany(freeWeightData);
+const colorData = [
+    {
+        name: 'Red',
+        hex: '#ff0000',
+        rgb: '255, 0, 0'
+    },
+    {
+        name: 'Orange',
+        hex: '#ff6200',
+        rgb: '255, 98, 0'
+    },
+    {
+        name: 'Yellow',
+        hex: '#ffff00',
+        rgb: '255, 255, 0'
+    },
+    {
+        name: 'Green',
+        hex: '#00ff00',
+        rgb: '0, 255, 0'
+    },
+    {
+        name: 'Blue',
+        hex: '#0000ff',
+        rgb: '0, 0, 255'
+    },
+    {
+        name: 'Purple',
+        hex: '#8800ff',
+        rgb: '136, 0, 255'
+    },
+    {
+        name: 'Brown',
+        hex: '#4d2c02',
+        rgb: '77, 44, 2'
+    },
+    {
+        name: 'Black',
+        hex: '#000000',
+        rgb: '0, 0, 0'
+    },
+    {
+        name: 'Grey',
+        hex: '#585858',
+        rgb: '88, 88, 88'
+    },
+    {
+        name: 'White',
+        hex: '#ffffff',
+        rgb: '255, 255, 255'
+    },
+    {
+        name: 'Gold',
+        hex: '#e0ad04',
+        rgb: '224, 173, 4'
+    },
+    {
+        name: 'Silver',
+        hex: '#cfcfcf',
+        rgb: '207, 207, 207'
+    }
+];
 
-//   console.log('all done!');
-//   process.exit(0);
-// });
+const componentTypeData = [
+    {
+        componentType: 'Bead'
+    },
+    {
+        componentType: 'Charm'
+    },
+    {
+        componentType: 'Chain'
+    },
+    {
+        componentType: 'Pendant'
+    },
+    {
+        componentType: 'Bead Cap'
+    },
+    {
+        componentType: 'Cabochon'
+    },
+];
+
+const countryData = [
+    {
+        countryName: 'China'
+    },
+    {
+        countryName: 'Japan'
+    },
+];
+
+const jewelryTypeData = [
+    {
+        jewelryType: 'Bracelet'
+    },
+    {
+        jewelryType: 'Necklace'
+    },
+    {
+        jewelryType: 'Earrings'
+    },
+    {
+        jewelryType: 'Ring'
+    }
+];
+
+const layoutData = [
+    {
+
+    }
+];
+
+const materialData = [
+    {
+        type: '',
+        pricePerOunce: 0.0
+    }
+];
+
+const shapeData = [
+    {
+        type: 'Circle'   
+    },
+    {
+        type: 'Oval'   
+    },
+    {
+        type: 'Teardrop'   
+    },
+    {
+        type: 'Square'   
+    },
+    {
+        type: 'Rectangle'   
+    },
+    {
+        type: 'Diamond'   
+    },
+    {
+        type: 'Heart'   
+    },
+    {
+        type: 'Star'   
+    },
+];
+const stoneData = [
+    {
+        type: '',
+        pricePerOunce: 0.0
+    }
+];
+
+const styleData = [
+    {
+        type: '',
+        background: '',
+        styleElements: '',
+        timePeriod: '',
+        notes: ''
+    }
+];
+//insert everything into the database with the models
+//const createColor = await Color.collection.insertMany(colorData);
+    Promise.all([Color.collection.insertMany(colorData),
+        ComponentType.collection.insertMany(componentTypeData),
+        Country.collection.insertMany(countryData),
+        JewelryType.collection.insertMany(jewelryTypeData),
+        Layout.collection.insertMany(layoutData),
+        Material.collection.insertMany(materialData),
+        Shape.collection.insertMany(shapeData),
+        Stone.collection.insertMany(stoneData),
+        Style.collection.insertMany(styleData)
+    ]);
+
+   console.log('all done!');
+  process.exit(0);
+ });
