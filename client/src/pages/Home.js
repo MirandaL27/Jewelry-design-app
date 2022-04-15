74c3df5010454459984c73f3331883512bc54c28
Cadenza <mirandalucsb@gmail.com>
2022-04-14 18:48:06 -0700

    added styling to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,33 +7,34 @@ function Homepage() {
         <Container className='bg-image' style={{ height: '100vh' }} fluid>
             <Col className='bg-overlay' style={{ width: '100%', height: '100%' }}>
                 <Row className="d-flex justify-content-center" style={{ width: '100%' }}>
-                    <h2>Welcome</h2>
+                    <h2 className="maroon-background pt-2 pb-2 pl-2 pr-5 rounded">Welcome</h2>
                 </Row>
                 <Row className="d-flex justify-content-center" style={{ width: '100%' }}>
-                    <h4>This app is intedned to help you create impeccable jewelry designs. </h4>
-                </Row>
-                <Row className="d-flex justify-content-center" style={{ width: '100%' }}>
-                    <Row style={{ width: '60%' }}>
-                        <Col className="m-5 p-5">
-                            <h3>Getting Started: </h3>
-                            <p>Click on the settings link to configure dropdown options</p>
-                            <p>(including, but not limited to): </p>
-                            <ul>
+                    <Row style={{ width: '80%' }}>
+                        <Col className="m-5 p-5 ornate-border">
+                            <div className="maroon-background p-2 rounded">
+                            <h4>Getting Started: </h4>
+                            <p className = "small">Click on the settings link to configure dropdown options</p>
+                            <p className = "small">(including, but not limited to): </p>
+                            <ul className = "small list-style">
                                 <li>Color</li>
-                                <li>Material (e.g. gold, silver, copper etc.)</li>
                                 <li>Style (e.g. victorian, gothic, boho etc.)</li>
-                                <li>Stone (e.g. amethyst, topaz, diamond, etc.)</li>
                                 <li>Component Type (e.g. bead, bead cap, connector, charm etc.)</li>
-                                <li>Jewelry Type (e.g. bracelet, necklace etc.)</li>
                             </ul>
+                            </div>
                         </Col>
 
-                        <Col className="m-5 p-5">
-                            <h3>Usage: </h3>
-                            <p>Click the components link to add jewelry components.</p>
-                            <p>Click the images link to add inspirational images.</p>
-                            <p>Click the designs link to combine jewelry components into a design.</p>
-                            <p>Click the collage link to create image collages from your inspirational images.</p>
+                        <Col className="m-5 p-5 ornate-border">
+                            <div className="maroon-background p-2 rounded">
+                            <h4>Usage: </h4>
+                            <ul className = "small list-style">
+                                <li>Click the components link to add jewelry components.</li>
+                                <li>Click the images link to add inspirational images.</li>
+                                <li>Click the designs link to combine jewelry components into a design.</li>
+                                <li>Click the collage link to create image collages from your inspirational images.</li>
+                            </ul>
+                            </div>
+                            
                         </Col>
                     </Row>
 
