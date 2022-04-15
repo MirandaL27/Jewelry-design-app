c400475302a6589008872e1baccdb167e44ba6cc
Cadenza <mirandalucsb@gmail.com>
2022-04-15 01:05:00 -0700

    added conditionally rendering add component button

--- client/src/pages/Component.js
+++ client/src/pages/Component.js
@@ -1,16 +1,21 @@
 import React, {useState} from "react";
 import { Container, Col, Row, Button, Collapse } from 'react-bootstrap';
 import ComponentFilter from '../components/ComponentFilter';
+import Auth from '../utils/auth';
+
 function Component() {
-
-
     return (
         <Container className='maroon-background' style={{ minHeight: '100vh' }} fluid>
                 <Row fluid>
                     <div className="text-center" style={{width: '100%'}}>
-                        <h2>Search Components</h2>
+                        <h2>Components</h2>
                     </div>  
                 </Row>
+                {Auth.loggedIn() && <Row fluid>
+                    <div className = "d-flex justify-content-end m-5" style={{width: '100%'}}>
+                    <Button className="small gold-background maroon-font red-shadow-hover transition no-border red-bg-active">Add Component</Button>
+                    </div>
+                </Row>}
                 <Row className="bg-red" fluid>
                     <ComponentFilter className="collapse" id="ComponentFilter"/>
                 </Row>
