3cbf3a1f9610a7b3af6776f8e2c7c190fdf402ce
Cadenza <mirandalucsb@gmail.com>
2022-04-15 00:52:17 -0700

    collapsable filter

--- client/src/components/ComponentFilter/index.js
+++ client/src/components/ComponentFilter/index.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card, Collapse, Button, Fade } from 'react-bootstrap';
+function ComponentFilter() {
+    const [open, setOpen] = useState(false);
 
-function Footer() {
     return (
-        <div className = "bg-red gold-font card card-body">
-            <p className = "small p-3 m-0">Search Filter stuff here!</p>
-        </div>  
+
+        <div className="p-3 m-3 text-center" style={{width: '100%'}}>
+            <Button className="small gold-background maroon-font red-shadow-hover transition no-border red-bg-active" onClick={() => setOpen(!open)}
+                aria-controls="filterDiv"
+                aria-expanded={open}>Filter Search
+            </Button>
+            <div>
+                <Collapse in={open} >
+                        <div className="bg-red gold-font text-center rounded" id="filterDiv">
+                            <Card body  className="bg-red" style={{ width: '100%' }}>
+                                <p className="small">Filter stuff goes here!</p>
+                                <Button>Test</Button>
+                            </Card>
+                        </div>
+                </Collapse>
+            </div>
+        </div>
     );
 }
 
-export default Footer;
+export default ComponentFilter;
--- client/src/pages/Component.js
+++ client/src/pages/Component.js
@@ -1,30 +1,23 @@
-import React from "react";
-import { Container, Col, Row, Button } from 'react-bootstrap';
+import React, {useState} from "react";
+import { Container, Col, Row, Button, Collapse } from 'react-bootstrap';
 import ComponentFilter from '../components/ComponentFilter';
 function Component() {
+
+
     return (
-        <Container className='maroon-background' style={{ height: '100vh' }} fluid>
-            <Row>
-            <Col className = "bg-red" >
-                <Row>
-                    <Button data-toggle="collapse" data-target="#ComponentFilter" aria-expanded="false" aria-controls="ComponentFilter">Filter Search</Button>
+        <Container className='maroon-background' style={{ minHeight: '100vh' }} fluid>
+                <Row fluid>
+                    <div className="text-center" style={{width: '100%'}}>
+                        <h2>Search Components</h2>
+                    </div>  
                 </Row>
-                <Row>
+                <Row className="bg-red" fluid>
                     <ComponentFilter className="collapse" id="ComponentFilter"/>
                 </Row>
-            </Col>
-            <Col>
-                <Row>
-                    <div className="d-flex justify-content-center">
-                        <h2>Search Components</h2>
-                    </div>
-                </Row>
-                <Row className="ornate-border">
+                <Row className="ornate-border m-3">
                     {/*images of components go here!*/}
                     Image components
                 </Row>
-            </Col>
-            </Row>
 
         </Container>
     );
